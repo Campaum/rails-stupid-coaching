4dfa5d34fabb766255c1baec3d5de6891c4ca56d
Camille Paumier <cam.paumier@gmail.com>
2019-05-16 15:07:24 +0200

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+class QuestionsController < ApplicationController
+
+  ANSWERS = {
+    1 => "Great!",
+    2 => "Silly question, get dressed and go to work!",
+    3 => "I don't care, get dressed and go to work!"
+  }
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = ANSWERS[1]
+    elsif @question.match(/\?|\w+[?]/)
+      @answer = ANSWERS[2]
+    else
+      @answer = ANSWERS[3]
+    end
+  end
+end
